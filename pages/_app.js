dcb179c6dc01ac053c5558c333b0c6bf07eff355
romeo <romeomeshack34@gmail.com>
2022-11-30 12:28:26 +0100

    Modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css"
 
-export default function App({Component, pagesProps}) {
-  return <Component {...pagesProps}/>
+export default function App({Component, pageProps}) {
+  return <Component {...pageProps}/>
 }
