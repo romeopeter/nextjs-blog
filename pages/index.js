ba3e4db9b2c842d29e645102d0afd5bb139a01c8
romeo <romeomeshack34@gmail.com>
2022-12-01 03:25:51 +0100

    Added static props

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,58 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/_utils.module.css";
 
-export default function Home() {
+import { getSortedBlogPostData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedBlogPostData();
+
+  return {
+    props: {
+      allPostsData: JSON.parse(allPostsData),
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>
-          Hi, I'm Romeo 👋🏿 <br /> I'm a Software developer, Technical writer and Creator.
+          Hi, I'm Romeo 👋🏿 <br /> I'm a Software developer, Technical writer and
+          Creator.
         </p>
+
         <p>
-          This is a
-          <a href="https://nextjs.org/learn"> Next.js</a> sample website.
+          This is a<a href="https://nextjs.org/learn"> Next.js</a> sample
+          website.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, matterResult }) => {
+            
+            const title = matterResult.data.title;
+            const date = matterResult.data.date;
+
+            console.log(matterResult);
+
+            return (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </Layout>
   );
